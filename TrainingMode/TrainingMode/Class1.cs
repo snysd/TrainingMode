7ad2793528f53859deea67ee0f6b64a2d0df3159
DESKTOP-1VP9R0T\sny2s <sny2sd@gmail.com>
2022-01-15 20:39:55 +0900

    gitignore追加

---
+++ TrainingMode/TrainingMode/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrainingMode
+{
+    class Class1
+    {
+        public static void Elevator()
+        {
+            int N = Int32.Parse(Console.ReadLine());
+            int[] Array = new int[N];
+            for (var i = 0; i < N; i++)
+            {
+                Array[i] = Int32.Parse(Console.ReadLine());
+            }
+            int sum = 0;
+            for (var i = 0; i < N; i++)
+            {
+                int M = 0;
+                int m = 0;
+                if (i == 0) m = 1;
+                else m = Array[i - 1];
+                M = m - Array[i];
+                if (M < 0) M = M * (-1);
+                sum += M;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ TrainingMode/TrainingMode/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TrainingMode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            Console.WriteLine($"Hello {rand.Next()}!");
+            Console.ReadKey();
+            Class1.Elevator();
+        }
+    }
+}
